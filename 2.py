0650588f4db600e794d30af9233427863038de3e
Ivan <31912106+Shihkauskas@users.noreply.github.com>
2017-12-05 18:52:20 +0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,19 @@
+import re
+
 with open('text.txt', 'r', encoding='utf8') as file:
     text = file.read().split(' ')
+
 result = []
 for slovo in text:
-    if len(slovo) > 6:
+
+    #Чуть извращения
+    template_search = re.compile(r"\w+(?=[,.])")
+    x = template_search.findall(str(text))
+
+    if len(slovo) > 6 and slovo != x:
         result.append(slovo.capitalize())
     else:
         result.append(slovo)
 
-with open('resultat.txt', 'w', encoding='utf8') as final_file:
-    final_file.write(' '.join([str(i) for i in result]))
+    with open('resultat.txt', 'w', encoding='utf8') as final_file:
+        final_file.write(' '.join([str(i) for i in result]))
